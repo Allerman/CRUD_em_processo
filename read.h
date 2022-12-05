aa9a36f1abf78abe5dc460198f81f7889a87c7df
Allerman <Allerman3301@gmail.com>
2022-12-05 20:36:09 -0300

    nova alteração

--- read.h
+++ read.h
@@ -8,14 +8,15 @@
 void ler(cadastro *pessoas) {
   printf(
       "\n================================================================\n");
-  printf("\t\t\t\t\t\t EXIBIR CADASTRO");
+  printf("\t\t\tEXIBIR CADASTRO");
   printf("\n============================================================="
          "===\n");
-  printf("O código da pessoa é: %s\n", pessoas->hospital);
   printf("O nome da pessoa é: %s\n", pessoas->nome);
-  printf("A idade da pessoa é: %i\n", pessoas->cod_cons);
-  printf("O e-mail da pessoa é: %s\n", pessoas->data);
-  sleep(5);
+  printf("O codigo da consulta é: %i\n", pessoas->cod_cons);
+  printf("O nome do hospital é: %s\n", pessoas->hospital);
+  printf("A data da consulta é: %s\n", pessoas->data);
+  
+  sleep(8);
   system("pause");
   system("clear");
 }
